133a97375382b740dbabde37588c1d7c9f4be645
Andrew Massey <acmassey@ncsu.edu>
2021-07-29 15:15:16 -0400

    Complete iteration 1

---
+++ lib/book.rb
@@ -0,0 +1,14 @@
+class Book
+
+  attr_reader :title,
+              :author,
+              :publication_year
+
+  def initialize(info_hash)
+    @title = info_hash[:title]
+    @author = info_hash[:author_first_name] + " " + info_hash[:author_last_name]
+    @publication_year = info_hash[:publication_date].chars.last(4).join
+  end
+
+
+end
